ce2fb40d4fef57c9eb784394a017bbc311f51611
Sulaiman-SK <slmlm600@gmail.com>
2021-10-12 02:24:21 +0300

    fixed

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import sys
 try :
 	os.system("echo start setting up ...")
 	os.system("sudo apt update && upgrade")
-	os.system("sudo apt-get install -y openjdk-11-jdk")
+	os.system("sudo apt-get install -y openjdk-17-jdk")
 	os.system("echo finish installing java")
 	os.system("sudo apt-get update & upgrade")
 	os.system("sudo apt-get install -y flex bison gcc g++ make build-essential")
@@ -21,10 +21,10 @@ try :
 	os.system("cd ~")
 	os.system("mkdir Tools")
 	os.system("cd Tools")
-	# os.system("wget http://xsb.sourceforge.net/downloads/XSB.tar.gz")
-	# os.system("tar xzf XSB.tar.gz")
-	# os.system("wget https://people.cs.ksu.edu/~xou/argus/software/mulval/mulval_1_1.tar.gz")
-	# os.system("tar xzf mulval_1_1.tar.gz")
+	os.system("wget http://xsb.sourceforge.net/downloads/XSB.tar.gz")
+	os.system("tar xzf XSB.tar.gz")
+	os.system("wget https://people.cs.ksu.edu/~xou/argus/software/mulval/mulval_1_1.tar.gz")
+	os.system("tar xzf mulval_1_1.tar.gz")
 	os.system("mv mulval Tools/")
 	os.system("mv XSB Tools/")
 	os.system("cd Tools/")
@@ -33,7 +33,7 @@ try :
 
 		add this path to this file ".bashrc"
 
-		export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64
+		export JAVA_HOME=/usr/lib/jvm/java-1.17.0-openjdk-amd64
 		export PATH=$PATH:$HOME/bin:$JAVA_HOME/bin
 
 		export PATH=/home/**type your username**/Tools/XSB/bin:$PATH
